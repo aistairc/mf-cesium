1b0f7bd59f1f1e00c7fb8048caec31fc1b8130ce
jungham <jungham97@naver.com>
2017-01-25 16:15:32 +0900

    first git

---
+++ hyemi/js/model/Dictionary.js
@@ -0,0 +1,28 @@
+var CellDictionary = {};//points of cell  for change of style
+var NetworkDictionary = {};//points of network for change of style
+var BoundaryDictionary = {};
+
+var PlaceHolder = {};
+var AllGeometry = {};//mesh of cell,mesh of network for change of visible
+var Information = {}; //cells
+var StateInformation = {}; //states
+var TransitionInformation = {}; //transtions
+var BoundaryInformation = {};
+var GmlIdMap = {};
+var viewer;
+
+var stair = {};
+var door = {};
+var room = {};
+var corridor = {};
+
+var stairline = {};
+var doorline = {};
+var roomline = {};
+var corridorline = {};
+
+var ControlVisible = {};
+var ControlVisibleLine = {};
+var Index = {};
+var preIndex = {};
+var Gaia = [];
