49b49e34d089e53ad9c9f2094b6164cc7f2544c0
wijaecho <wijae0512@gmail.com>
2023-08-25 16:03:15 +0900

    Remove the previous version of mf-cesium

--- Stinuum Web/js/mf-cesium/src/sh_makeJS.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-cat model.js direction_radar.js	movement_draw.js geometry_viewer.js	occurrence_map.js super.js path_drawing.js temporal_map.js mf_collection.js	property_graph.js utility.js query_processor.js > ../stinuum.js
